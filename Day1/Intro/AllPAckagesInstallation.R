3da5a5ff650830b92f516a313e75f4bbb3f58807
Lucia Michielin Comis <lucia.michielin.comis@gmail.com>
2023-05-29 13:29:27 +0100

    Edit on installations files

---
+++ Day1/Intro/AllPAckagesInstallation.R
@@ -0,0 +1,38 @@
+# Data handling general
+install.packages("tidyverse")
+
+# Web scraping 
+library("rvest")
+install.packages("rvest")
+# Text Analysis
+
+install.packages("tm")
+install.packages("lexicon")
+install.packages("wordcloud")
+install.packages("textstem")
+install.packages("topicmodels")
+install.packages("quanteda")
+install.packages("quanteda.textstats")
+install.packages("quanteda.textmodels")
+install.packages("quanteda.textplots")
+
+# Sentiment Analysis 
+install.packages("syuzhet")
+
+#Statistics
+install.packages("ggfortify")
+install.packages("lme4")
+install.packages("effects")
+install.packages("sjPlot")
+
+# Data Vis
+install.packages("gganimate")
+install.packages("gifski")
+install.packages("av")
+install.packages("latticeExtra")
+install.packages("viridis")
+install.packages("rgdal")
+install.packages("sp")
+install.packages("sf")
+install.packages("gridExtra")
+install.packages("RColorBrewer")
---
+++ Day1/Intro/InstallationNoteable.R
@@ -0,0 +1,23 @@
+# Installation of the package needed for the week 
+# Please run this during the first block of the summer school and put a red sticky on your laptop if any of this throws an error
+# NB Since we are working via Noteable we do not need to install all the packages only those that are not yet in the system 
+
+# Text Analysis
+install.packages("lexicon")
+install.packages("wordcloud")
+install.packages("textstem")
+install.packages("quanteda.textstats")
+install.packages("quanteda.textmodels")
+install.packages("quanteda.textplots")
+
+# Sentiment Analysis 
+install.packages("syuzhet")
+install.packages("vader")
+
+#Statistics
+install.packages("ggfortify")
+
+
+# Data Vis
+install.packages("gifski")
+install.packages("av")
