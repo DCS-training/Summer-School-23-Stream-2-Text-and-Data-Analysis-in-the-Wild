c176068ed35fc4087547df933de52b15a9f73648
jess witte <123657953+jessicawitte92@users.noreply.github.com>
2023-06-01 15:22:34 +0100

    added exercises

--- Day3/SentimentAnalysis/SentimentPractical.R
+++ Day3/SentimentAnalysis/SentimentPractical.R
@@ -1,3 +1,6 @@
+# Exercise 1: Filtering, analysing and plotting data  =============
+#With your table, create a plot for a subset of your choice (e.g. r/UK posts from 2022, r/Scotland posts with compound >=0.5, etc). Discuss your findings
+#Then, share your plot with the class.
 
 # Exercise Subset 
 UK23<-UK %>%
@@ -13,3 +16,32 @@ ggplot(UK23, aes(x = date, y =Sentiment, colour=Sentiment)) +
   labs(title='r/UK VADER Compound Sentiment', x = "Post Date", y = "Sentiment Score") +
   theme_minimal()+
   geom_hline(yintercept=0, linetype='dotted')
+
+# Exercise 2: Comparing positive + negative results  =============
+# With your table, repeat the process for the r/UK datset. Then, discuss your findings.
+## 5.1. Subset the top 25 positive and top 25 negative posts---------
+top_positive <- SentimentUK[1:25, ]
+top_negative <- SentimentUK[(nrow(SentimentUK)-24):nrow(SentimentUK), ]
+## 5.2. Clean and tokenize ------
+# First the subset positive data:
+library(quanteda)
+pos_subset <- gsub("http\\S+\\s*", "", top_positive)
+pos_subset <- tolower(pos_subset)
+pos_subset <- quanteda::tokens(pos_subset)
+pos_subset <- tokens_remove(pos_subset, stopwords("en"))
+pos_subset <- tokens_select(pos_subset, stopwords("en"), selection = "remove")
+pos_subset <- tokens_select(pos_subset, min_nchar = 3)
+## 5.3. Generate the word cloud -----
+wordcloud(pos_subset,
+          random.order=FALSE,
+          max.words=100,
+          colors=brewer.pal(n=5, name = "Dark2"))
+#Repeat for the subset negative data:
+neg_subset <- gsub("http\\S+\\s*", "", top_negative)
+neg_subset <- tolower(neg_subset)
+neg_subset <- quanteda::tokens(neg_subset)
+neg_subset <- tokens_remove(neg_subset, stopwords("en"))
+neg_subset <- tokens_select(neg_subset, stopwords("en"), selection = "remove")
+neg_subset <- tokens_select(neg_subset, min_nchar = 3)
+# Generate the word cloud
+wordcloud(neg_subset, random.order=FALSE, max.words=100, colors=brewer.pal(n=5, name = "Set2"))
